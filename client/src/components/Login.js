98f4617f2b21396a665f97fed65d91d45cc713d9
unknown <vegasergiojr@gmail.com>
2021-10-11 13:37:48 -0500

    created the skeleton of login and register page with minimal styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,9 +3,22 @@ import React from 'react'
 function Login() {
     return (
         <div className="login-page">
-            <input type="text" placeholder="username..."></input>
-            <input type="text" placeholder="password..."></input>
-            <button type="submit">Login</button>
+            <div className="login">
+                <h3>Login</h3>
+                <input className="input-fields" name="username" type="text" placeholder="username..."></input>
+                <input className="input-fields" name="password" type="text" placeholder="password..."></input>
+                <button type="submit">Login</button>
+            </div>
+            <div className="register">
+                <h3>Register</h3>
+                <input className="input-fields" type="text" placeholder="First Name"></input>
+                <input className="input-fields" type="text" placeholder="Last Name"></input>
+                <input className="input-fields" type="text" placeholder="username"></input>
+                <input className="input-fields" type="text" placeholder="Email"></input>
+                <input className="input-fields" tpye="password" placeholder="password"></input>
+                <input className="input-fields" type="password" placeholder="password"></input>
+                <button>Register</button>
+            </div>
         </div>
     )
 }
