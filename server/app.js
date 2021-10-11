41483f8f4182bb4d2c985a334bb310aa6a685758
Heriberto Lopez Jr <hl_9023@yahoo.com>
2021-10-11 09:26:28 -0500

    first commit

---
+++ server/app.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const models = require('./models')
+const cors = require('cors')
+const db = require('./models')
+const app = express() 
+const jwt = require('jsonwebtoken')
+const authenticate = ('./middleware/authMiddleware')
+const PORT = process.env.PORT || 8080
+
+require('dotenv').config()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.post('/api/post', (req, res) => {
+
+    const body_text = req.body.body_text
+    const image = req.body.image 
+
+
+    const post = models.Post.build({
+        body_text: body_text, 
+        image: image, 
+    
+    })
+
+    // save a post
+    post.save()
+    .then(savedPost => {
+        res.json({success: true, postId: savedPost.id})
+    })
+
+})
+
+app.get('/api/post', authenticate, (req, res) => {
+    // get all posts
+    models.PostfindAll({})
+    .then(posts => {
+        res.json(posts)
+    })
+})
+
+app.delete('/api/post/:postId', (req, res) => {
+
+    const postd = parseInt(req.params.postId) 
+
+    models.Post.destroy({
+        where: {
+            id: postId
+        }
+    }).then(_  => {
+        res.json({success: true})
+    })
+
+})
+
+
+
+app.listen(PORT, (req, res) => {
+    console.log('Server is running...')
+})
